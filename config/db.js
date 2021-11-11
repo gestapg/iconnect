9e7a0840680370ec247422848dfc0010a8c16546
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-11-11 16:18:16 +0700

    User Registration

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
     });
-    console.log("Database Connected...");
+    console.log('Database Connected...');
   } catch (err) {
     console.error(err.message);
     // Exit proccess with failure
