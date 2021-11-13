830518f4e04864c2e4ca2ee5cb63706ff6897176
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-11-13 21:20:40 +0700

    Alert Component and action call

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,31 @@ import Navbar from './components/Layouts/Navbar';
 import Landing from './components/Layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/Layouts/Alert';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route path="/" exact component={Landing} />
-        <section className="container">
-          <Switch>
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route path="/" exact component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
